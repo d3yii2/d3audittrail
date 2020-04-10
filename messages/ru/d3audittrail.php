bf3f3c53f1915dbc9b7669354bbd4f3402ecb2fe
uldisn <uldisnelsons@gmail.com>
2020-04-10 18:23:51 +0300

    Added RU translation

---
+++ messages/ru/d3audittrail.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'Audit trail' => 'Контрольный журнал',
+    'Model ID' => 'Модель',
+    'User ID' => 'ID пользователя',
+    'Action' => 'Действие',
+    'Audit trail records' => 'Записи контрольного журнала',
+    'User' => 'пользователь',
+    'Full Name' => 'Имя Фамилия',
+    'Stamp' => 'Время',
+    'Old Value' => 'Предыдущее значение',
+    'Time' => 'Время',
+    'New Value' => 'Новое значение',
+    'Field' => 'поле',
+    'Back' => 'Atgriezties',
+    'Model' => 'Модель',
+    'ID' => 'ID',
+    'Audittrail' => 'Контрольный журнал',
+    'Audittrail data' => 'Данные аудита',
+    'User Name' => 'Имя пользователя'
+];
